6fc41248a29a0c3cf35dcbfc59bef96777a44501
Ty Schrock <ty.schrock14@gmail.com>
2020-01-07 10:47:43 -0500

    starting sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Sidebar, Menu, Icon, Checkbox } from "semantic-ui-react"
+import "./Sidebar.css"
 
 function App() {
   return (
-    <div>
-     
+    <div id= "sidebar">
+     <Sidebar
+      as={Menu}
+      animation='overlay'
+      icon='labeled'
+      inverted
+      vertical
+      visible
+      width='thin'
+    >
+      <Checkbox label='Make my profile visible' />
+      <Menu.Item as='a'>
+        <Icon name='home' />
+        Home
+      </Menu.Item>
+      <Menu.Item as='a'>
+        <Icon name='gamepad' />
+        Games
+      </Menu.Item>
+      <Menu.Item as='a'>
+        <Icon name='camera' />
+        Channels
+      </Menu.Item>
+    </Sidebar>
+
     </div>
   );
 }
