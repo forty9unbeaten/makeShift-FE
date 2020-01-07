0c875f15646fdb74ec261b1792cb10161a822e82
Ybrayym <ybrayymjan90@gmail.com>
2020-01-07 13:42:27 -0500

    NavBar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import {Tab} from "./react/components"
+import { MyTab } from './react/components/Tab.js'
+
+
+function App() {
+  return (
+    <div>
+      <MyTab />
+    </div>
+  );
+}
+
+export default App;
