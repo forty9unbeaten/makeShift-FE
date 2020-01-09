e31e840cc6a5819c846e4d698184103b03c5f1e1
Ybrayym <ybrayymjan90@gmail.com>
2020-01-09 09:15:41 -0500

    Search Bar && Header Body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import  MyMenu  from './react/components/MyMenu.js'
 import MenuBody from './react/components/MyMenuBody.js'
+import SearchBody from './react/components/Search.js'
 
 
 function App() {
   return (
     <div>
+      {/* <SearchBody /> */}
       <MyMenu />
       <MenuBody />
     </div>
--- src/react/components/MyMenuBody.js
+++ src/react/components/MyMenuBody.js
@@ -1,25 +1,37 @@
 import React from 'react'
-import { Item } from 'semantic-ui-react'
+// import { Item } from 'semantic-ui-react'
 import "./MyMenuBody.css"
 
 
 const description = [
-    'Website Description'
-  ].join(' ')
-  
-  const MenuBody = () => (
-    <Item.Group>
-      <Item>
-        {/* <Item.Image size='small' src='https://react.semantic-ui.com/images/wireframe/image.png' /> */}
-        <Item.Content>
-          {/* <Item.Header as='a'>Cute Dog</Item.Header> */}
-          <Item.Description>
-            <p>{description}</p>
-            <p>3d printer catalogue</p>
-          </Item.Description>
-        </Item.Content>
-      </Item>
-    </Item.Group>
-  )
-  
-  export default MenuBody
+    'Website Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed adipiscing diam donec adipiscing tristique risus nec. Integer malesuada nunc vel risus commodo viverra maecenas.'
+].join(' ')
+
+// const MenuBody = () => (
+//     <Item.Group>
+//         <Item>
+//             {/* <Item.Image size='small' src='https://react.semantic-ui.com/images/wireframe/image.png' /> */}
+//             <Item.Content>
+//                 {/* <Item.Header as='a'>Cute Dog</Item.Header> */}
+//                 <Item.Description>
+class MyMenuBody extends React.Component {
+    render() {
+        return (
+            <div className="menuBody">
+                <div className="menuDesc">
+                    <h2>Header</h2>
+                    <div className="webDescription">
+                    <p>{description}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+//                 </Item.Description>
+//             </Item.Content>
+//         </Item>
+//     </Item.Group>
+// )
+
+export default MyMenuBody
