90a2e2959bf2034368d0b782745f7e8791a5ad41
Rob Spears <rob.sp3ars@gmail.com>
2020-01-13 13:51:53 -0500

    Restructure ProductCard component

--- src/react/components/ProductCard.js
+++ src/react/components/ProductCard.js
@@ -1,41 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-// const product = props.product;
-
-const ProductCard = props => {
-  return (
-    <Card style={{ width: "225px" }}>
-      <Image
-        style={{
-          height: "225px",
-          width: "225px",
-          overflow: "hidden",
-          margin: "auto"
-        }}
-        src={props.product.productImgs[0]}
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>{props.product.productName}</Card.Header>
-        <Card.Meta>
-          <span className="category">{props.product.productCategory}</span>
-        </Card.Meta>
-        <Card.Description>{props.product.productDescription}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <span class="left floated like">
-          <i class="like icon"></i>
-          Like
-        </span>
-        <span class="right floated star">
-          <i class="star icon"></i>
-          Favorite
-        </span>
-      </Card.Content>
-    </Card>
-  );
-};
+export class ProductCard extends Component {
+  render() {
+    return (
+      <Card style={{ width: "225px" }}>
+        <Image
+          style={{
+            height: "225px",
+            width: "225px",
+            overflow: "hidden",
+            margin: "auto"
+          }}
+          src={this.props.imgs[0]}
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>
+            <Link to={`/details/${this.props.id}`}>{this.props.name}</Link>
+          </Card.Header>
+          <Card.Meta>
+            <span className="category">{this.props.category}</span>
+          </Card.Meta>
+          <Card.Description>{this.props.description}</Card.Description>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
 
 export default ProductCard;
