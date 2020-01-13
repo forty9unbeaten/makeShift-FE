b97750aa4ac6864e4b3e37c1cbf5f29f8bc2979a
Rob Spears <rob.sp3ars@gmail.com>
2020-01-13 10:18:38 -0500

    Implement MenuBody/header on Home page

--- src/react/components/MenuBody.js
+++ src/react/components/MenuBody.js
@@ -1,36 +1,24 @@
 import React from "react";
-// import { Item } from 'semantic-ui-react'
+
 import "./MenuBody.css";
 
-const description = [
-  "Website Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed adipiscing diam donec adipiscing tristique risus nec. Integer malesuada nunc vel risus commodo viverra maecenas."
-].join(" ");
-
-// const MenuBody = () => (
-//     <Item.Group>
-//         <Item>
-//             {/* <Item.Image size='small' src='https://react.semantic-ui.com/images/wireframe/image.png' /> */}
-//             <Item.Content>
-//                 {/* <Item.Header as='a'>Cute Dog</Item.Header> */}
-//                 <Item.Description>
 class MyMenuBody extends React.Component {
   render() {
     return (
       <div className="menuBody">
         <div className="menuDesc">
-          <h2>Header</h2>
+          <h2>3D Printing for Home DIY</h2>
           <div className="webDescription">
-            <p>{description}</p>
+            <p>
+              With the help of our growing community of makers and designers,
+              search our free catalog of 3D print filesto meet any of your
+              household needs.
+            </p>
           </div>
         </div>
       </div>
     );
   }
 }
-//                 </Item.Description>
-//             </Item.Content>
-//         </Item>
-//     </Item.Group>
-// )
 
 export default MyMenuBody;
