04c1001a23bd8f1f431f2ede09f5c820e2b36d94
Rob Spears <rob.sp3ars@gmail.com>
2020-01-14 14:55:13 -0500

    Clean up code and imports

--- src/react/components/Menu.js
+++ src/react/components/Menu.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Menu } from "semantic-ui-react";
 import SearchBar from "./Search.js";
 import "./Menu.css";
-import {Link} from 'react-router-dom'
-
+import { Link } from "react-router-dom";
 
 class MyMenu extends React.Component {
   state = {};
@@ -15,28 +14,24 @@ class MyMenu extends React.Component {
 
     return (
       <Menu className="mainMenu">
-        
-        <Link to='/category'>
-          
-        <Menu.Item
-          name="CATALOG"
-          active={activeItem === "CATALOG"}
-          onClick={this.handleItemClick}
-        >
-          CATALOG
-        </Menu.Item>
-        </Link>
-
-        <Link to='/'>
-        <Menu.Item
-          name="HOME"
-          active={activeItem === "HOME"}
-          onClick={this.handleItemClick}
+        <Link to="/category">
+          <Menu.Item
+            name="CATALOG"
+            active={activeItem === "CATALOG"}
+            onClick={this.handleItemClick}
           >
-          HOME
-        </Menu.Item>
+            CATALOG
+          </Menu.Item>
         </Link>
-
+        <Link to="/">
+          <Menu.Item
+            name="HOME"
+            active={activeItem === "HOME"}
+            onClick={this.handleItemClick}
+          >
+            HOME
+          </Menu.Item>
+        </Link>
         <SearchBar />
         <div className="logo">
           <div className="logo__icon"></div>
