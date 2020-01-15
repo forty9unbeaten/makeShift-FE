9d3b82ecda592610de61e6f0847c697153b17956
Rob Spears <rob.sp3ars@gmail.com>
2020-01-15 10:14:09 -0500

    Add action creator and reducer for category filter

--- src/redux/stateReducers/index.js
+++ src/redux/stateReducers/index.js
@@ -4,5 +4,6 @@ import * as products from "./products";
 export const masterReducer = combineReducers({
   allProducts: products.getAllProducts,
   singleProduct: products.getSingleProduct,
-  newProduct: products.postProduct
+  newProduct: products.postProduct,
+  filters: products.filterCategories
 });
