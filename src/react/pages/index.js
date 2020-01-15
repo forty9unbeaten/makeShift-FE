c7316b45c29014c364eedcc472033a2690d265f4
Ty Schrock <ty.schrock14@gmail.com>
2020-01-15 11:48:06 -0500

    error page added

--- src/react/pages/index.js
+++ src/react/pages/index.js
@@ -1,10 +1,11 @@
 import Home from "./Home";
 import Detail from "./Detail";
 import Category from "./Category";
+import NotFound from "./NotFound"
 
 export default {
   Home: { path: "/", component: Home },
   Detail: { path: "/details/:id", component: Detail },
-  Category: { path: "/category", component: Category }
-  
+  Category: { path: "/category", component: Category },
+  NotFound: { path: "*", component: NotFound }  
 };
