38f43df9a37c77b4f1e999ec5b29089c56eea3e9
Rob Spears <rob.sp3ars@gmail.com>
2020-01-24 16:50:21 -0500

    Add Login modal

--- src/react/components/NavBar.js
+++ src/react/components/NavBar.js
@@ -1,15 +1,39 @@
 import React from "react";
-import { Menu } from "semantic-ui-react";
+import { Menu, Modal } from "semantic-ui-react";
 import SearchBar from "./Search.js";
 import "./NavBar.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 class NavBar extends React.Component {
-  state = { activeItem: "HOME" };
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: "HOME",
+      loginOpen: false,
+      username: "",
+      password: ""
+    };
+  }
 
   handleClick = event => {
     const clickedLink = event.target.innerHTML;
     this.setState({ activeItem: clickedLink });
+  };
+
+  handleChange = event => {
+    if (event.target.name === "username") {
+      this.setState({ username: event.target.value });
+    } else {
+      this.setState({ password: event.target.value });
+    }
+  };
+
+  openLogin = () => {
+    this.setState({ loginOpen: true });
+  };
+
+  closeLogin = () => {
+    this.setState({ loginOpen: false });
   };
 
   render() {
@@ -27,14 +51,6 @@ class NavBar extends React.Component {
         <div className="navbar__linksWrapper">
           <NavLink
             exact
-            to="/login"
-            className="navbar__link"
-            activeClassName="active"
-          >
-            Login
-          </NavLink>
-          <NavLink
-            exact
             to="/"
             className="navbar__link"
             activeClassName="active"
@@ -49,6 +65,52 @@ class NavBar extends React.Component {
           >
             Catalog
           </NavLink>
+          <Modal
+            trigger={
+              <div onClick={this.openLogin} className="navbar__link">
+                Login/Register
+              </div>
+            }
+            open={this.state.loginOpen}
+            onClose={this.closeLogin}
+            className="Login__modal"
+          >
+            <Modal.Header className="Login__header">Log In</Modal.Header>
+            <Modal.Content>
+              <form className="Login__form">
+                <div className="Login__input">
+                  <label htmlFor="username">Username:</label>
+                  <input
+                    name="username"
+                    type="text"
+                    autoComplete="username"
+                    onChange={this.handleChange}
+                    value={this.state.username}
+                  />
+                </div>
+                <div className="Login__input">
+                  <label htmlFor="password">Password:</label>
+                  <input
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    onChange={this.handleChange}
+                    value={this.state.password}
+                  />
+                </div>
+                <div className="Login__button" onClick={this.closeLogin}>
+                  Submit
+                </div>
+              </form>
+              <div className="Login__register">
+                Don't have an account?
+                <br />
+                <Link to="/register" onClick={this.closeLogin}>
+                  Register
+                </Link>
+              </div>
+            </Modal.Content>
+          </Modal>
         </div>
       </Menu>
     );
