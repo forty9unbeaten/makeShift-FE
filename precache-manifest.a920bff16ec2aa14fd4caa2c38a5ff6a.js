088a7f81100dee9466d8e05c7c9c0804df0f3aca
Rob Spears <rob.sp3ars@gmail.com>
2020-02-06 12:15:14 -0500

    Updates

---
+++ precache-manifest.a920bff16ec2aa14fd4caa2c38a5ff6a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb841a90863eacef22cf4a4f47493438",
+    "url": "/makeShift-FE/index.html"
+  },
+  {
+    "revision": "4abd6cb40eb381bac88b",
+    "url": "/makeShift-FE/static/css/main.b1e9ee23.chunk.css"
+  },
+  {
+    "revision": "55aa16c19d41a1b95abc",
+    "url": "/makeShift-FE/static/js/2.913c0aa9.chunk.js"
+  },
+  {
+    "revision": "f8513fb9f2981a1b206fb733b985f272",
+    "url": "/makeShift-FE/static/js/2.913c0aa9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4abd6cb40eb381bac88b",
+    "url": "/makeShift-FE/static/js/main.7c55befa.chunk.js"
+  },
+  {
+    "revision": "a55663070f944d3f500e",
+    "url": "/makeShift-FE/static/js/runtime-main.e85259b2.js"
+  },
+  {
+    "revision": "1f0760e5fa48f937f59d354d0a152744",
+    "url": "/makeShift-FE/static/media/facepalm.1f0760e5.gif"
+  },
+  {
+    "revision": "3d45c224697a58f9cc1b56299cc9a6fc",
+    "url": "/makeShift-FE/static/media/hero.3d45c224.png"
+  },
+  {
+    "revision": "f95f4ef3f4722c75adf0574078dc61b2",
+    "url": "/makeShift-FE/static/media/logo.f95f4ef3.png"
+  },
+  {
+    "revision": "403e0b113ccb4ba5db5f9de4f73e6b11",
+    "url": "/makeShift-FE/static/media/logo_white.403e0b11.png"
+  }
+]);
