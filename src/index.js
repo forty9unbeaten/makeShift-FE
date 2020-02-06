5a9d989e90dc3853501cd366641f617642103cf8
Rob Spears <rob.sp3ars@gmail.com>
2020-02-06 12:20:11 -0500

    Config project for deployment

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ export const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>,
